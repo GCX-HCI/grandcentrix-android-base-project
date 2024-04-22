89043c764e5e0d10e01aa0c90a0b0707e7833e9f
Fabian Vorholt <30832848+fvorholt@users.noreply.github.com>
2024-04-22 15:39:03 +0200

    [Feature] Add core design module (#9)

---
+++ app/src/main/java/net/grandcentrix/baseProjectSetup/MainActivity.kt
@@ -0,0 +1,38 @@
+package net.grandcentrix.baseProjectSetup
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import net.grandcentrix.core.design.theme.AppTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+
+        setContent {
+            AppTheme {
+            }
+        }
+    }
+}
+
+@Composable
+fun AndroidApp(modifier: Modifier = Modifier) {
+    Scaffold(modifier = modifier) {
+        Text("Hello grandcentrix", modifier = Modifier.padding(it))
+    }
+}
+
+@androidx.compose.ui.tooling.preview.Preview
+@Composable
+private fun AndroidAppPreview() {
+    AppTheme {
+        AndroidApp()
+    }
+}
