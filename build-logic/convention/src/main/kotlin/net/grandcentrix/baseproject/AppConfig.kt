ded34666c28f7c648a65eb0bf93bf7f17bae07bf
Fabian Vorholt <30832848+fvorholt@users.noreply.github.com>
2024-11-04 10:35:48 +0100

    Bump compile and target sdk to 35 (#59)

--- build-logic/convention/src/main/kotlin/net/grandcentrix/baseproject/AppConfig.kt
+++ build-logic/convention/src/main/kotlin/net/grandcentrix/baseproject/AppConfig.kt
@@ -2,8 +2,8 @@ package net.grandcentrix.baseproject
 
 object AppConfig {
     const val MIN_SDK = 26
-    const val COMPILE_SDK = 34
-    const val TARGET_SDK = 34
+    const val COMPILE_SDK = 35
+    const val TARGET_SDK = 35
 
     const val VERSION_NAME = "1.0"
     const val VERSION_CODE = 1
